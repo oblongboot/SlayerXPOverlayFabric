8be21424be2abfae0d20b11cf0a62b7db0b31b89
neon-kat <georgealanbutler8@gmail.com>
2025-08-11 16:31:00 +0100

    shit somewhat works i think, gui time soontm!

--- src/main/java/com/slayerxp/overlay/mixin/MixinContributerColor.java
+++ src/main/java/com/slayerxp/overlay/mixin/MixinContributerColor.java
@@ -10,29 +10,30 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.List;
 
-@Mixin(TextHandler.class)
-public class MixinContributerColor {
-
-    @ModifyVariable(
-        method = "wrapLines(Lnet/minecraft/text/StringVisitable;ILnet/minecraft/text/Style;)Ljava/util/List;",
-        at = @At("HEAD"),
-        index = 1,
-        argsOnly = true
-    )
-    private StringVisitable modifyWrapLinesStringVisitable(StringVisitable input) {
-        StringVisitable transformed = ContributerColors.INSTANCE.transformStringVisitable(input);
-        return transformed != null ? transformed : input;
-    }
-
-    @ModifyVariable(
-        method = "wrapLines(Lnet/minecraft/text/StringVisitable;ILnet/minecraft/text/Style;Ljava/util/function/BiConsumer;)V",
-        at = @At("HEAD"),
-        index = 1,
-        argsOnly = true
-    )
-    private StringVisitable modifyWrapLinesStringVisitableWithConsumer(StringVisitable input) {
-        StringVisitable transformed = ContributerColors.INSTANCE.transformStringVisitable(input);
-        return transformed != null ? transformed : input;
-    }
-
-}
+//@Mixin(TextHandler.class)
+//public class MixinContributerColor {
+//
+//    @ModifyVariable(
+//        method = "wrapLines(Lnet/minecraft/text/StringVisitable;ILnet/minecraft/text/Style;)Ljava/util/List;",
+//        at = @At("HEAD"),
+//        index = 1,
+//        argsOnly = true
+//    )
+//    private StringVisitable modifyWrapLinesStringVisitable(StringVisitable input) {
+//        StringVisitable transformed = ContributerColors.INSTANCE.transformStringVisitable(input);
+//        return transformed != null ? transformed : input;
+//    }
+//
+//    @ModifyVariable(
+//        method = "wrapLines(Lnet/minecraft/text/StringVisitable;ILnet/minecraft/text/Style;Ljava/util/function/BiConsumer;)V",
+//        at = @At("HEAD"),
+//        index = 1,
+//        argsOnly = true
+//    )
+//    private StringVisitable modifyWrapLinesStringVisitableWithConsumer(StringVisitable input) {
+//        StringVisitable transformed = ContributerColors.INSTANCE.transformStringVisitable(input);
+//        return transformed != null ? transformed : input;
+//    }
+//
+//}
+// on hold rn while i fix shit
