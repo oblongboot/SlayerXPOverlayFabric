88c1db53b99e146058dafa0b7f7dd70079ffac9c
oblongboot <81798709+oblongboot@users.noreply.github.com>
2026-05-29 16:55:52 +0100

    chore: cleanup stub features and cleanup sxpdevcommand

--- src/main/kotlin/dev/oblongboot/sxp/commands/impl/SXPDevCommand.kt
+++ src/main/kotlin/dev/oblongboot/sxp/commands/impl/SXPDevCommand.kt
@@ -42,6 +42,7 @@ object SXPDevCommand {
     private fun executeClient(context: CommandContext<FabricClientCommandSource>): Int {
         val devSetting = StringArgumentType.getString(context, "devSetting")
         val debug = StringArgumentType.getString(context, "debug")
+
         if (devSetting == "simslayer") {
             onMessage.handleSlayerQuestStart(true)
             modMessage("starting sim, ends in 50 ticks")
@@ -52,40 +53,6 @@ object SXPDevCommand {
             APIUtils.getXP()
             modMessage("getting xp")
         }
-        if (devSetting == "test1") {
-            FeatureManager.loadAllFeatureStates()
-        }
-        if (devSetting == "test2") { Config.toggle(debug) }
-        if (devSetting == "scoreboarddebug") { val e = Scoreboard.getSlayerType(); modMessage("Scoreboard area: $e") }
-        if (devSetting == "gui") {bleh()}
-        if (devSetting == "armorstand") {
-            val armorStands = getArmorStands()
-            for (stand in armorStands) {
-                println(stand.name.toString());
-                println(stand.uuid.toString());
-            }
-        }
-        if (devSetting == "bossinfo") {
-            val parts = mutableListOf<String>()
-            val options = Config.getMultiSelect("BossInfoCheckbox")
-            if (options.contains(0)) {
-                parts.add("Slayer XP: 69,420,676")
-            }
-            if (options.contains(1)) {
-                parts.add("Kills: 40")
-            }
-            if (options.contains(2)) {
-                parts.add("Time: 31s, Boss: 15s, Spawn: 16s")
-            }
-            if (options.contains(3)) {
-                parts.add("KPH: 20")
-            }
-            modMessage(parts.joinToString(" | "))
-        }
-        if (devSetting == "testgradient") {
-            Minecraft.getInstance().player?.displayClientMessage(getGradientStyleMessage(debug, 0x00D9FF, 0xFF6B35), false)
-        }
-        modMessage("/sxpdev devsetting:$devSetting debug:$debug")
         return 1
     }
 }
